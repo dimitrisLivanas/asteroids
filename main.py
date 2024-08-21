3ee9a1e64a24bc3949f8f4cd5fb72c9a5526760e
dimitrisLivanas <livanas.dimitris@gmail.com>
2024-08-21 17:42:19 +0300

    create a new GUI window

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from constants import *
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode(SCREEN_WIDTH, SCREEN_HEIGHT)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
