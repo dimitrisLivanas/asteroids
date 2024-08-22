3c9d04b0956264e713021b38f73dc3fcc3101bff
dimitrisLivanas <livanas.dimitris@gmail.com>
2024-08-22 14:41:05 +0300

    generate asteroids

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -12,9 +14,13 @@ def main():
     clock = pygame.time.Clock()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
     # create Player instance AFTER adding Player to the groups
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    asteroidField = AsteroidField()
     while True:
         dt = clock.tick(60)/1000
         for event in pygame.event.get():
