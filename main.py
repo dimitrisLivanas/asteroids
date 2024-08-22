310b3ea0e70516fd54826b648182d1efbc5b246e
dimitrisLivanas <livanas.dimitris@gmail.com>
2024-08-22 13:52:45 +0300

    move player with w, s

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 20)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     while True:
         dt = clock.tick(60)/1000
         for event in pygame.event.get():
